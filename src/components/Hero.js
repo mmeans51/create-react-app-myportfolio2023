50bc0e9a6e0440b821595a67dfcdb4cd576e088c
michael Means <colemeans51@gmail.com>
2023-01-23 18:02:53 -0600

    set up

---
+++ src/components/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+    return (
+        <div className='hero'>
+            <div className='content'>
+                <p></p>
+                <p></p>
+                <p>Michael Means</p>
+                <p>Software Developer</p>
+                <button href='/' className='button'>Contact</button>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
