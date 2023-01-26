033dc3947d61b0128e5d51040ce612e243af3f74
michael Means <colemeans51@gmail.com>
2023-01-25 18:18:07 -0600

    save

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,78 +1,83 @@
-import React, { useState } from 'react'
-import { FaBars, FaTimes } from 'react-icons/fa'
-import logo from './images/Michael Means-logos_white.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import logo from "./images/Michael Means-logos_white.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faLinkedin,
-    faGithub,
-    faInstagram
-  } from '@fortawesome/free-brands-svg-icons'
-import './Navbar.css'
+  faLinkedin,
+  faGithub,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import "./Navbar.css";
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
 
-    const [click, setClick] = useState(false)
-    const handleClick = () => setClick(!click)
+  const closeMenu = () => setClick(false);
 
-    const closeMenu = () => setClick(false)
-
-    return (
-        <div className='header'>
-            <nav className='navbar'>
-                <a href='/' className='logo'>
-                    <img src={logo} alt='logo' />
-                </a>
-                <div className='hamburger' onClick={handleClick}>
-                    {click ? (<FaTimes size={30} style={{ color: '#ffffff' }} />)
-                        : (<FaBars size={30} style={{ color: '#ffffff' }} />)}
-
-                </div>
-                <ul className={click ? "nav-menu active" : "nav-menu"}>
-                    <li className='nav-item'>
-                        <a href='/' onClick={closeMenu}>Home</a>
-                    </li>
-                    <li className='nav-item'>
-                        <a href='#about' onClick={closeMenu}>About</a>
-                    </li>
-                    <li className='nav-item'>
-                        <a href='#myworklist' onClick={closeMenu}>My Work</a>
-                    </li>
-                    <li className='nav-item'>
-                        <a href='#contact' onClick={closeMenu}>Contact Me</a>
-                    </li>
-                </ul>
-            </nav>
-            <ul>
-        <li>
-          <a
-            href="https://www.linkedin.com/in/michael-means-340240113/"
-            // target="_blank"
-            rel="noreferrer"
-          >
-            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
-          </a>
-        </li>
-        <li>
+  return (
+    <div className="header">
+      <nav className="navbar">
+        <a href="/" className="logo">
+          <img src={logo} alt="logo" />
+        </a>
+        <div className="hamburger" onClick={handleClick}>
+          {click ? (
+            <FaTimes size={30} style={{ color: "#ffffff" }} />
+          ) : (
+            <FaBars size={30} style={{ color: "#ffffff" }} />
+          )}
+        </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <a href="/" onClick={closeMenu}>
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#about" onClick={closeMenu}>
+              About
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#myworklist" onClick={closeMenu}>
+              My Work
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#contact" onClick={closeMenu}>
+              Contact Me
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="https://www.linkedin.com/in/michael-means-340240113/"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+            </a>
+          </li>
+          <li className="nav-item">
           <a
             href="https://github.com/mmeans51"
-            // target="_blank"
             rel="noreferrer"
           >
             <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
           </a>
+          </li>
+          <li className="nav-item">
           <a
             href="https://www.instagram.com/cole_means/"
-            // target="_blank"
             rel="noreferrer"
-          >
+            >
             <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" />
           </a>
         </li>
-        <li>
-        </li>
-      </ul>
-        </div>
-    )
-}
+        </ul>
+      </nav>
+      
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
